eef41952616b46aa71682003907228b605456da2
G22DevShadow <g22devshadow@gmail.com>
2026-05-19 21:15:58 -0500

    first version

---
+++ src/main/java/com/school/roller_speed/controllers/NavController.java
@@ -0,0 +1,14 @@
+package com.school.roller_speed.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class NavController {
+
+    @GetMapping("/")
+    public String getMethodName() {
+        return "index";
+    }
+}
